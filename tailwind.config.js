7fbe86e5822836f58f06e5b944fa1e9453659291
Kawser Simanto <kawsersimanto@gmail.com>
2024-09-26 00:57:57 +0600

    setup theme and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          DEFAULT: "1140px",
+        },
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#B4F461",
+          secondary: "#F9F7F3",
+          accent: "rgba(17, 17, 17, 0.05)",
+          neutral: "#111111",
+          "accent-40": "rgba(17, 17, 17, 0.4)",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
