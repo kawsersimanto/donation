92b39ff5fdd749e390ad352a198257655fb69f83
kawserimanto <kawsersimanto@gmail.com>
2024-09-27 00:41:30 +0600

    added sticky header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     colors: {
       "accent-40": "rgba(17, 17, 17, 0.4)",
+      "accent-30": "rgba(17, 17, 17, 0.30)",
       "accent-70": "rgba(17, 17, 17, 0.70)",
     },
     extend: {
@@ -23,6 +24,7 @@ module.exports = {
           primary: "#B4F461",
           secondary: "#F9F7F3",
           accent: "rgba(17, 17, 17, 0.05)",
+          accent30: "rgba(17, 17, 17, 0.30)",
           neutral: "#111111",
           "base-100": "#ffffff",
         },
